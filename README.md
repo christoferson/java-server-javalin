8af07612dde2e9dc4c6970f95ad5a9bb1de94fc0
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-10-31 17:02:24 +0900

    Add RouteOverviewPlugin and Global ExceptionHandler

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # java-server-javalin
 Java Server Javalin
+
+
+
+### RouteOverviewPlugin
+
+```
+config.plugins.register(new RouteOverviewPlugin("/overview"));
+```
+
+http://localhost:7001/overview
+
+
+### Exception Handling
+
+
+```
+app.exception(ApplicationException.class, (e, ctx) -> {
+	  ctx.status(500);
+	  ctx.result(e.getLocalizedMessage());
+});
+```
+
+http://localhost:7001/error/throw
--- src/main/java/demo/DemoServerJavalin.java
+++ src/main/java/demo/DemoServerJavalin.java
@@ -1,19 +1,28 @@
 package demo;
 
+import demo.exceptions.ApplicationException;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import io.javalin.plugin.bundled.RouteOverviewPlugin;
 
 public class DemoServerJavalin {
 
 	public static void main(String[] args) {
 
 	    Javalin app = Javalin.create(config -> {
-	    	config.http.defaultContentType = "application/json";
+	    	//config.http.defaultContentType = "application/json";
+	    	config.plugins.register(new RouteOverviewPlugin("/overview"));
 	    });
 	    app.get("/", ctx -> ctx.result("Foo Bar " + System.currentTimeMillis()));
 	    app.get("/static/method", DemoServerJavalin::mstatic);
 	    app.get("/static/lambda", DemoServerJavalin.mstaticl);
+	    app.get("/error/throw", ctx -> { throw new ApplicationException("Error " + System.currentTimeMillis()); });
+	    
+	    app.exception(ApplicationException.class, (e, ctx) -> {
+	    	  ctx.status(500);
+	    	  ctx.result(e.getLocalizedMessage());
+	    });
 
 	    app.start(7001);
 	    
